de898898701bdedfa4a98bc0dad84588162c6dae
it-21293962 <99237217+it-21293962@users.noreply.github.com>
2024-03-10 19:43:41 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose'; // Correct import for MongooseModule
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
+    // Correct usage of MongooseModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
